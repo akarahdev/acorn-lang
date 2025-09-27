68ee31bf12f3ad65482262a6bea0ebf5f2294d4c
akarahdev <endisticbutitsforgit@gmail.com>
2025-09-26 22:23:42 -0400

    fix: fix compiler warnings

--- src/main/java/acorn/Main.java
+++ src/main/java/acorn/Main.java
@@ -1,14 +1,8 @@
 package acorn;
 
-import acorn.parser.Parser;
-import acorn.parser.ctx.GlobalContext;
-import acorn.token.Tokenizer;
 import acorn.ui.CLI;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.*;
-import java.util.Objects;
-import llvm4j.module.Module;
 import picocli.CommandLine;
 
 public class Main {
